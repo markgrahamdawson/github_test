b7069895810f02406c337758f26c789da2543b8c
Mark Dawson <mark.dawson@metoffice.gov.ukpython>
2023-12-28 08:22:19 +0000

    made test.py dynamic

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
-def test_function():
-    '''This function adds two constant values.'''
-    a = 1
-    b = 2
+def test_function(a, b):
+    '''This function adds two values.'''
     return a+b
 
-print(test_function())
+print(test_function(1,2))
 print('the end')
